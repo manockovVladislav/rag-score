d055711c04ad7a9442d39ebdd6261a9a01e63864
manockovVladislav <manockov@outlook.com>
2026-02-04 23:13:31 +0300

    create stable version

--- src/rag_eval/__init__.py
+++
@@ -1,6 +0,0 @@
-"""RAG evaluation toolkit based on ragas."""
-
-from .registry import rag_system, get_rag_system, load_registries
-from .notebook import run_eval_notebook
-
-__all__ = ["rag_system", "get_rag_system", "load_registries", "run_eval_notebook"]
